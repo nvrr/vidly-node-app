8378562abe0daf0052b4b49d33b96274ab270d19
nvrr <sureshvarma809@gmail.com>
2022-09-03 06:11:40 -0700

    bug fixes and installed mongoose ntg else

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.get('/', (req,res)=>{
     res.send('Hello expressjs')
 })
 
-app.use('api/generes', generes);
+app.use('/api/generes', generes);
 
 const port = process.env.PORT || 3000;
 app.listen(port, ()=> console.log(`listening on ${port} port....`))
