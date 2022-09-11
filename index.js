b747226ee74f8f1c3e0ceb351617319d6682ab3b
nvrr <sureshvarma809@gmail.com>
2022-09-10 22:28:27 -0700

    Authenticating user

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const Joi = require('joi')
 Joi.objectId = require('joi-objectid')(Joi)
 const mongoose = require("mongoose");
 const users = require('./routes/users')
+const auth = require('./routes/auth')
 const rentals = require('./routes/rentals')
 const movies = require('./routes/movies')
 const customers = require('./routes/customers')
@@ -28,6 +29,7 @@ app.use('/api/customers', customers);
 app.use('/api/movies', movies);
 app.use('/api/rentals', rentals);
 app.use('/api/users', users);
+app.use('/api/auth', auth);
 
 const port = process.env.PORT || 3000;
 app.listen(port, ()=> console.log(`listening on ${port} port....`))
