843f61c8968abaaf5c4f5b06f78e70253916cf45
nvrr <sureshvarma809@gmail.com>
2022-09-11 09:20:32 -0700

    Authentication and Authorization: Authoriation Middleware

---
+++ middleware/auth.js
@@ -0,0 +1,18 @@
+ const jwt = require('jsonwebtoken')
+ const config = require('config')
+
+ module.exports = function (req,res,next){
+    const token = req.head('x-auth-token');
+    if(!token) return res.status(402).send('Access denied. No token provided.')
+
+   try {
+    const decoded = jwt.verify(token, config.get('jwtPrivateKey'))
+   req.user = decoded;
+   next();
+} 
+   catch(e){
+    res.status(400).send('Invalid token.',e)
+   }
+ }
+
+//  module.exports = auth;
